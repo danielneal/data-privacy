80efae17305151905014f7fb4391876c533cd4c8
Daniel Neal <danielneal@Daniels-MacBook-Air.local>
2020-12-10 12:23:24 +0000

    Save data as file

--- server.js
+++ server.js
@@ -1,6 +1,9 @@
+const { v4: uuidv4 } = require("uuid");
 let express = require("express");
 let mustacheExpress = require("mustache-express");
 let bodyParser = require("body-parser");
+let fs = require("fs");
+
 let app = express();
 
 app.set("views", `${__dirname}/views`);
@@ -8,9 +11,21 @@ app.set("view engine", "mustache");
 
 app.engine("mustache", mustacheExpress());
 
-app.get("/dataEntry", function (req, res) {
-  res.render("dataEntry", { pageTitle: "Data Privacy" });
+let db = {};
+
+app.get("/enterData", function (req, res) {
+  const id = uuidv4();
+  res.render("enterData", { id: id, pageTitle: "Data Privacy" });
 });
+
+app.post(
+  "/saveData",
+  express.urlencoded({ extended: true }),
+  function (req, res) {
+    let { id, data } = req.body;
+    fs.writeFileSync(`/tmp/${id}.json`, data);
+  }
+);
 
 app.listen(3000, function () {
   console.log("Server started");
